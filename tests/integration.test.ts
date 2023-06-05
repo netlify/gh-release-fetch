4a3a064027db0ee792306b3a30e6a85150155da9
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-06-05 14:01:30 +0200

    chore: solve rate limits

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -2,7 +2,9 @@ import { existsSync } from 'fs'
 import { readFile, rm } from 'fs/promises'
 import { Agent } from 'https'
 import { join } from 'path'
+import process from 'process'
 
+import { Headers } from 'node-fetch'
 import { temporaryDirectory } from 'tempy'
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 
@@ -18,6 +20,12 @@ describe('integration', () => {
     vi.resetAllMocks()
   })
 
+  const headers = new Headers()
+
+  if (process.env.GITHUB_TOKEN) {
+    headers.append('Authorization', `Bearer ${process.env.GITHUB_TOKEN}`)
+  }
+
   // Do as little tests as necessary here for now, as we run into GH rate limiting
 
   test('should download simple file with custom agent', async () => {
@@ -29,7 +37,7 @@ describe('integration', () => {
         destination: tmpDir,
         extract: false,
       },
-      { agent: new Agent({ keepAlive: false }) },
+      { agent: new Agent({ keepAlive: false }), headers },
     )
 
     const file = join(tmpDir, 'checksums.txt')
@@ -44,12 +52,15 @@ describe('integration', () => {
 
   test('should download an archive and extract it', async () => {
     const tmpDir = temporaryDirectory()
-    await fetchLatest({
-      ...baseRelease,
-      package: 'live-tunnel-client-linux-386.tar.gz',
-      destination: tmpDir,
-      extract: true,
-    })
+    await fetchLatest(
+      {
+        ...baseRelease,
+        package: 'live-tunnel-client-linux-386.tar.gz',
+        destination: tmpDir,
+        extract: true,
+      },
+      { headers },
+    )
 
     const file = join(tmpDir, 'live-tunnel-client')
 
